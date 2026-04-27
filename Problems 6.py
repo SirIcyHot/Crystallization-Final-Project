3c472857df01e0fe13de03a261fa4aa8c663dc9f
Augustus Sison <93167378+SirIcyHot@users.noreply.github.com>
2026-04-27 11:06:26 -0400

    Add comments for MOO strategy development in Problems 6.py
    Added comments outlining the development of a dynamic multiobjective optimisation strategy, including cost functions and Pareto front analysis.

---
+++ Problems 6.py
@@ -0,0 +1,6 @@
+#Develop a dynamic multiobjective optimisation (MOO) strategy by considering two
+#cost functions: maximising the mean crystal size and maximising yield. Formulate and
+#solve the MOO problem. Justify the algorithm/method used and show and comment
+#on the Pareto front. Suggest an operating point from the Pareto front and justify your
+#choice. Is it possible to consider an additional objective function? Justify your answer.
+
