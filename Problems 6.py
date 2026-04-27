e757f4fa785fe277b466e669c91c85c29a3a00cb
Augustus Sison <93167378+SirIcyHot@users.noreply.github.com>
2026-04-27 11:11:30 -0400

    Refine comments in Problems 6.py for clarity
    Updated comments to clarify the MOO problem and objectives.

--- Problems 6.py
+++ Problems 6.py
@@ -1,6 +1,17 @@
 #Develop a dynamic multiobjective optimisation (MOO) strategy by considering two
 #cost functions: maximising the mean crystal size and maximising yield. Formulate and
-#solve the MOO problem. Justify the algorithm/method used and show and comment
-#on the Pareto front. Suggest an operating point from the Pareto front and justify your
-#choice. Is it possible to consider an additional objective function? Justify your answer.
+#solve the MOO problem. Justify the algorithm/method used and show the Pareto front.
+#Suggest an operating point from the Pareto front and justify your
+#choice. Is it possible to consider an additional objective function?
+
+
+#constants
+b = 6.2
+g = 1.5
+T_0 = 315 #k
+k_g = 0.017
+k_b = 7.8*10^19
+
+
+#Two MOO cost functions. maximize mean crystal size, yield
 
