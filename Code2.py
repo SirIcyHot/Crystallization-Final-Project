86848390cdfd89fe8e718eac3982b63594047940
Augustus Sison <93167378+SirIcyHot@users.noreply.github.com>
2026-04-27 10:03:36 -0400

    Refactor Code2.py to improve structure and readability

--- Code2.py
+++ Code2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-
+#test
 # -----------------------------
 # Problem constants
 # -----------------------------
